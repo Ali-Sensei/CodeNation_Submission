bd24edbdc5076b944c0a660d13fca21f5215efac
CodeNation21 <codenation21@CodeNation21s-MacBook-Pro.local>
2019-03-01 14:43:07 +0000

    NavBar adjusted for small devices

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,9 @@
-/*
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
 }
-
-( function( $ ) {
-  // Init Skrollr
-  var s = skrollr.init({
-      render: function(data) {
-          //Debugging - Log the current scroll position.
-          //console.log(data.curTop);
-      }
-  });
-} )( jQuery );
-
-window.addEventListener("keydown", function(e) {
-  // space and arrow keys
-  if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
-      e.preventDefault();
-  }
-}, false);
-
-*/
